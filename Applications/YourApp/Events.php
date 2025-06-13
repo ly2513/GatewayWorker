6abc3f8eb77b8b7f22bf0659644b72260de97f8f
dylan.li <dylan.li@huolala.cn>
2025-06-13 12:23:07 +0800

    初始化

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -35,8 +35,13 @@ class Events
      * @param int $client_id 连接id
      */
     public static function onConnect($client_id) {
+        // 向当前client_id发送数据
+        Gateway::sendToClient($client_id, json_encode(array(
+            'type'      => 'init',
+            'client_id' => $client_id
+        )));
         // 向当前client_id发送数据 
-        Gateway::sendToClient($client_id, "Hello $client_id");
+        // Gateway::sendToClient($client_id, "Hello $client_id");
         // 向所有人发送
         Gateway::sendToAll("$client_id login");
     }
